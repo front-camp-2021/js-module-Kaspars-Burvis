efd8472fdb9c4925136b0eb2409e726370b83f51
kaspars <kaspars.kdf@gmail.com>
2021-09-30 15:39:42 +0300

    card & cardList

--- src/module-2/card/index.js
+++ src/module-2/card/index.js
@@ -21,7 +21,42 @@ export default class Card {
     this.render();
   }
 
+  getTemplate () {
+    return `	<div class="card shadow col-3 m-5">
+                <img src=${this.images[0]} class="card-img-top p-5" alt="apple-watch"></img>
+                 <div class="card-body">
+                  <div class="d-flex justify-content-between pb-2">
+                    <div class="bg-primary text-white p-1 rounded shadow">${this.rating}  <i class="bi bi-star"></i> </div>
+                    <div>&#36; ${this.price}</div>
+                  </div>
+                  <h5 class="card-title">${this.title}</h5>
+                  <p class="card-text">${this.category}</p>
+                </div> 
+                <div class="d-flex justify-content-center align-items-center">
+                  <a href="#" class="col-6 btn bg-secondary bg-opacity-10">
+                    <i class="bi bi-suit-heart"></i> WISHLIST</a>
+                  <a href="#" class="col-6 btn btn-primary">
+                    <i class="bi bi-bag-plus"></i> ADD TO CART</a>
+                </div>
+              </div>`;
+  }
+
   render () {
-    // ... your logic
+    const wrapper = document.createElement('div');
+
+    wrapper.innerHTML = this.getTemplate();
+
+    this.element = wrapper.firstElementChild;
+  }
+
+  remove () {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy () {
+    this.remove();
+    this.element = null;
   }
 }

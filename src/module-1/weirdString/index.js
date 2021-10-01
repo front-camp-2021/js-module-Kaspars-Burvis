be56ca24125cebc1e645db4674682be577584301
kaspars <kaspars.kdf@gmail.com>
2021-10-01 18:35:34 +0300

    module-1 weirdString

--- src/module-1/weirdString/index.js
+++ src/module-1/weirdString/index.js
@@ -1,1 +1,3 @@
-export const weirdString = (str = "") => {};
+export const weirdString = (str = "") => {
+  return str.split(' ').map(word => word.slice(0, -1).toUpperCase() + word.slice(-1).toLowerCase()).join(' ');
+};
